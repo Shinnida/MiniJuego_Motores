b61d64e73743f814da10848905fa29f3a78945ee
Shinnida <xzjesusxz123@gmail.com>
2025-08-24 23:49:01 -0500

    Avance Final

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private SpriteRenderer playerRenderer; 
+
+    void Start()
+    {
+        playerRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Shape"))
+        {
+            Sprite shapeSprite = other.GetComponent<SpriteRenderer>().sprite;
+            playerRenderer.sprite = shapeSprite;
+
+            Debug.Log("Cambiando FORMA → " + other.name);
+        }
+
+        if (other.CompareTag("Color"))
+        {
+            Color newColor = other.GetComponent<SpriteRenderer>().color;
+            playerRenderer.color = newColor;
+
+            Debug.Log("Cambiando COLOR → " + other.name);
+        }
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,59 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-
     private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private SpriteRenderer playerRenderer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal"); 
-        moveInput.y = Input.GetAxisRaw("Vertical"); 
-
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput = moveInput.normalized;
-        transform.Translate(moveInput * speed * Time.deltaTime);
+
+        if (moveInput != Vector2.zero)
+        {
+            DoRaycast();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+    }
+
+    void DoRaycast()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, moveInput, 5f);
+
+        // Dibujar SIEMPRE el rayo (aunque no toque nada)
+        Debug.DrawRay(transform.position, moveInput * 5f, Color.red);
+
+        if (hit.collider != null)
+        {
+            GameObject obj = hit.collider.gameObject;
+
+            Debug.Log(" Detectado: " + obj.name +
+                      " | Posición: " + obj.transform.position +
+                      " | Tag: " + obj.tag);
+
+            if (obj.CompareTag("Shape"))
+            {
+                Sprite s = obj.GetComponent<SpriteRenderer>().sprite;
+                Debug.Log("El jugador cambiaría su FORMA a: " + s.name);
+            }
+            else if (obj.CompareTag("Color"))
+            {
+                Color c = obj.GetComponent<SpriteRenderer>().color;
+                Debug.Log("El jugador cambiaría su COLOR a: " + c);
+            }
+        }
     }
 }
+
